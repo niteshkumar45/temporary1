3566de091da893da625c8bbcf1c5a73d0b1c43fc
nitesh <niteshkymar1@gmail.com>
2023-08-01 14:40:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, './.env') })
+const express = require('express')
+const app = express()
+const MYPORT = process.env.PORT || 3600
+
+app.get("/",(req,res)=>{
+    res.status(201).json({
+        name:"nitesh kumar prajapat",
+        city:"jaipur",
+        email:"nitesh@gmail.com"
+    })
+})
+
+app.get("/user",(req,res)=>{
+    res.status(201).json({
+        content:"hello world"
+    })
+})
+
+
+app.listen(3200,()=>{
+    console.log("the server is started ");
+})

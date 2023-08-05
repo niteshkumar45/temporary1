6a6d7bfd25ea8400482085093914d571000ae7da
nitesh <niteshkymar1@gmail.com>
2023-08-05 13:04:26 +0530

    jsdlkfjsd

--- index.js
+++ index.js
@@ -2,35 +2,27 @@ const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 const express = require('express')
 const app = express()
-const ejs = require('ejs')
 const MYPORT = process.env.PORT || 3600
-const jwt = require('jsonwebtoken')
 const cors = require("cors")
 const mongoose = require('mongoose')
-const mongodb = require('mongodb')
-
-mongoose.connect("mongodb+srv://97nitesh85:lBQpQbIWKv8CsCWn@cluster0.soys12h.mongodb.net/",{
+const options = {
     useNewUrlParser:true,
     useUnifiedTopology:true
-}).then(()=>{console.log("mongodb connect")}).catch(()=>{console.log("not connect mongodb")})
+}
+const mongodb = require('mongodb')
+// 97nitesh85 AkBObPN8FnW4MPEl 
+mongoose.connect("mongodb+srv://97nitesh85:AkBObPN8FnW4MPEl@cluster0.9q695fl.mongodb.net/test",options).then(()=>{console.log("mongodb connect")}).catch(()=>{console.log("not connect mongodb")})
 
 const userschema = new mongoose.Schema({
     name:String,
-    city:String,
-    email:String,
-    password:String
+    city:String
 })
 
 const usermodel = new mongoose.model("User",userschema)
-
 app.use(cors())
-app.set("view engine","ejs")
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
 
-app.get("/",async(req,res)=>{
-    // const data = await usermodel.find()
-    res.status(201).json({user:"le tera data"})
+app.get("/",(req,res)=>{
+    res.status(201).json({message:"homepage"})
 })
 
 app.get("/user",async (req,res)=>{
